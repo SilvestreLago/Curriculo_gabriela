41ac200b0247f2ae11484e8644583ebe9a777c9d
Silvestre <87388202+SilvestreLago@users.noreply.github.com>
2023-04-03 12:07:33 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function carregar(){
-    var txt = window.document.getElementById('text')
-    var data = new Date()
-    var hora = data.getHours()
-    txt.innerHTML = `AGORA SÃO ${hora} HORAS.`
-}
