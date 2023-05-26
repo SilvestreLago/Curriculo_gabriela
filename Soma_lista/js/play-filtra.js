b8df5b582b23bd9b1f0e5657dc1a7c62779266d8
Silvestre <87388202+SilvestreLago@users.noreply.github.com>
2023-05-25 22:55:49 -0300

    Add files via upload

---
+++ Soma_lista/js/play-filtra.js
@@ -0,0 +1,38 @@
+
+var filtro = document.querySelector('.filtro');
+
+filtro.addEventListener("input", function(){
+    var numeros = document.querySelectorAll(".numero");
+    var ref = this;
+
+    if (this.value.length > 0){
+
+        numeros.forEach(function(numero){
+            var tdResult = numero.querySelector(".resultado");
+            var resultado = tdResult.textContent;
+            
+            var expressao = new RegExp(ref.value, "i");
+
+            var tdResult1 = numero.querySelector(".primeiro");
+            var resultado1 = tdResult1.textContent;
+            
+            var expressao = new RegExp(ref.value, "i");
+
+            var tdResult2 = numero.querySelector(".segundo");
+            var resultado2 = tdResult2.textContent;
+            
+            var expressao = new RegExp(ref.value, "i");
+
+            if (!expressao.test(resultado) && !expressao.test(resultado1) && !expressao.test(resultado2)) {
+                numero.classList.add("invisivel");
+            }else {
+                numero.classList.remove("invisivel");
+            }
+        }); 
+    }   else {
+        numeros.forEach(function(numero){
+            numero.classList.remove("invisivel");
+        });
+    }
+    
+});
